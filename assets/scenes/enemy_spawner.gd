e0456bbbb0c96eb41c91dd1bdec348641e9577ea
Thesuperjuniper <benmcnicholas2008@gmail.com>
2024-12-29 21:27:10 -0800

    I forgot

---
+++ assets/scenes/enemy_spawner.gd
@@ -0,0 +1,14 @@
+extends Node2D
+var previouslocation
+var location
+var locations = [Vector2(0,64),Vector2(0,-64),Vector2(64,0),Vector2(-64,0)]
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+func SpawnMagnetable():
+	print("joghn")
